79706e7b89a72d78b9ae00e5b3706142eb7bb57a
gorutin0438 <tokumeisan0403@gmail.com>
2026-02-13 11:16:31 +0900

    ヘッダーからホームページに戻れる様に変更

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ console.log("script.js loaded");
 const sidemenuToggle = document.querySelector('.sidemenu-toggle');
 const sidemenu = document.querySelector('.sidemenu');
 
-sidemenuToggle.addEventListener('click', () => {
-    console.log('click')
+if (sidemenuToggle && sidemenu) {
+    sidemenuToggle.addEventListener('click', () => {
+        console.log('click');
+        sidemenu.classList.toggle('open');
+    });
+}
 
-    //sidemenu をクリックした時にopenクラスを追加する
-    sidemenu.classList.toggle('open')
-})
+// 詳細ページからトップへ戻る際のなめらかな遷移
+if (document.body.classList.contains('detail-page')) {
+    const logoLink = document.querySelector('.header-logo-small a[href="index.html"]');
+    if (logoLink) {
+        logoLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            const href = logoLink.getAttribute('href');
+            document.body.classList.add('page-transition-exit');
+            setTimeout(() => {
+                window.location.href = href;
+            }, 350);
+        });
+    }
+}
